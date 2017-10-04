395be1bbcd5e8c897b493dd5e2158b14dd720d9d
luanfernandes <luanfranciscojr@gmail.com>
2017-10-04 19:44:23 -0400

    refactore

--- app/src/main/java/com/nanodegree/popularmovies/service/module/ServiceModule.kt
+++ app/src/main/java/com/nanodegree/popularmovies/service/module/ServiceModule.kt
@@ -26,7 +26,7 @@ class ServiceModule {
     }
 
     private val baseUrl = " https://api.themoviedb.org/3/"
-    private val apiKey = "90e22adce606a0ac579f85127ab407c8"
+    private val apiKey = ""
 
 
     @Provides
